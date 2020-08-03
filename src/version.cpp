8f8c7b98d389e4b8b2c9b937504cf3786dc3c8ff
OTOCASH <35067983+otocash@users.noreply.github.com>
2020-08-03 22:15:34 +0800

    Update version.cpp

--- src/version.cpp
+++ src/version.cpp
@@ -36,7 +36,7 @@ const std::string CLIENT_NAME("Otocash");
 // git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#    define GIT_COMMIT_ID "5f20c5f"
+#    define GIT_COMMIT_ID "8c851f4"
 #    define GIT_COMMIT_DATE "$Format:%cD"
 #endif
 
